b90f0e5cca1badb3737d3b6ca9d9ed20287aa171
ahdustin <ahdustin@f996e428-fef6-11de-89cc-8daf83ecd611>
2010-01-13 21:28:57 +0000

    Convenience ground objects

---
+++ trunk/Model/Convenience Objects/Ground.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+
+namespace Spectrum.Model
+{
+    class Ground : GameObject
+    {
+		/* Convenience to create an object with Affected By Gravity = NO, Pickupable = NO */
+		public Ground(int id, Colors viewableColors, Polygon polygon, Texture2D image, Vector2 position, Vector2 velocity, List<GameObject> combineObjects, List<GameObject> combinableWith, bool inactive, Texture2D inactiveImage, List<Events> events, bool existsWhenNotViewed, Level container) {
+			
+			base(id, viewableColors, polygon, image, position, false, velocity, combineObjects, combinableWith, false, inactive, inactiveImage, events, existsWhenNotViewed, container)
+		}
+    }
+}
---
+++ trunk/Model/Convenience Objects/SolidGround.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+
+namespace Spectrum.Model
+{
+    class SolidGround : GameObject
+    {
+		/* Convenience to create an object with Affected By Gravity = NO, Pickupable = NO, Viewable in All Colors, and Exists When Not Viewed */
+		public SolidGround(int id, Polygon polygon, Texture2D image, Vector2 position, Vector2 velocity, List<GameObject> combineObjects, List<GameObject> combinableWith, List<Events> events, Level container) {
+			
+			base(id, Colors.AllColors, polygon, image, position, false, velocity, combineObjects, combinableWith, false, false, null, events, true, container)
+		}
+    }
+}
