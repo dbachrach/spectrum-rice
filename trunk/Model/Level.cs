0a4e392386e98900ed6600169fbd9d201204ff9e
nsb1@rice.edu <nsb1@rice.edu@f996e428-fef6-11de-89cc-8daf83ecd611>
2010-01-13 20:39:52 +0000

    added to Level.cs

--- trunk/Model/Level.cs
+++ trunk/Model/Level.cs
@@ -2,31 +2,39 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
 
 namespace Spectrum.Model
 {
     class Level
     {
-        public int Id 
-        {
-            get { return id; }
-            set { id = value; }
-        }
-        private int id;
+        // a unique id for this level
+        public int Id { get; set; }
 
-        public int Number
-        {
-            get { return number; }
-            set { number = value; }
-        }
-        private int number;
+        // the number of this level in the sequence of the levels
+        public int Number { get; set; }
 
-        public string Name
-        {
-            get { return name; }
-            set { name = value; }
-        }
-        private string name;
+        // the name of the level
+        public string Name { get; set; }
 
+        // width of the level
+        public int Width { get; set; }
+
+        // height of the level
+        public int Height { get; set; }
+
+        // flag whether this level has been completed
+        public bool Completed { get; set; }
+        
+        // the background image of this level
+        public Texture2D Background { get; set; }
     }
 }
