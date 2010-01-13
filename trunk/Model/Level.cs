bd6c1cc32f8358605996d453617045f4222c4568
nsb1@rice.edu <nsb1@rice.edu@f996e428-fef6-11de-89cc-8daf83ecd611>
2010-01-13 20:25:00 +0000

    Added level class

---
+++ trunk/Model/Level.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Spectrum.Model
+{
+    class Level
+    {
+        public int Id 
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        private int id;
+
+        public int Number
+        {
+            get { return number; }
+            set { number = value; }
+        }
+        private int number;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        private string name;
+
+    }
+}
