09d59af0aa6dd2de26adf708c15c05623a15818b
ahdustin <ahdustin@f996e428-fef6-11de-89cc-8daf83ecd611>
2010-01-13 20:57:46 +0000

    Added Event Class

---
+++ trunk/Model/Event.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+
+namespace Spectrum.Model
+{
+	enum EventTypes { EventTypeXEvent, EventTypeBehvaior, EventTypeCollision }
+	
+    class Event
+    {
+        public EventType Type { get; set; }
+		public string DisplayName { get; set; }
+		public List<Action> Actions { get; set; }
+
+        public Event(EventType type, string displayName, List<Action> actions)
+        {
+            Type = type;
+			DisplayName = displayName;
+			Actions = actions;
+        }
+    }
+}
