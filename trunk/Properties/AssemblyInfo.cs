cddb579342a18877c5d15c97e9d187f707caa1d9
nsb1@rice.edu <nsb1@rice.edu@f996e428-fef6-11de-89cc-8daf83ecd611>
2010-01-13 20:18:33 +0000

    Windows game set up properly

---
+++ trunk/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Spectrum")]
+[assembly: AssemblyProduct("Spectrum")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("G6LK1L1")]
+
+[assembly: AssemblyCopyright("Copyright © G6LK1L1 2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e06d025f-b565-4e38-bbd6-1fa6a24c7be2")]
+
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
