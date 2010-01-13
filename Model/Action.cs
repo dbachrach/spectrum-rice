53a479e1f99cccaaa9fc59ad9a10d11608896bcf
ahdustin <ahdustin@f996e428-fef6-11de-89cc-8daf83ecd611>
2010-01-13 21:05:48 +0000

    Added Action Class

---
+++ Model/Action.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+
+namespace Spectrum.Model
+{
+	enum ActionType { ActionTypeChange, ActionTypeIncrement, ActionTypeDecrement }
+	
+    class Action
+    {
+        public GameObject Receiver { get; set; }
+		public string Property { get; set; }
+		public ActionType Type { get; set; }
+		public string Value { get; set; }
+		public bool Animated { get; set; }
+		public int AnimationDuration { get; set; }
+		public int Delay { get; set; }
+		public bool Repeats { get; set; }
+		public bool RepeatDelay { get; set; }
+
+        public Action(GameObject receiver, string property, ActionType type, string value, bool animated, int animationDuration, int delay, bool repeats, bool repeatDelay)
+        {
+            Receiver = receiver;
+			Property = property;
+			Type = type;
+			Value = value;
+			Animated = animated;
+			AnimationDuration = animationDuration;
+			Delay = delay;
+			Repeats = repeats;
+			RepeatDelay = repeatDelay;
+		}
+    }
+}
