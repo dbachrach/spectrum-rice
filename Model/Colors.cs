847bd6a70a0832de0da8e8fa002fc74bcbd3b7e0
ahdustin <ahdustin@f996e428-fef6-11de-89cc-8daf83ecd611>
2010-01-14 18:36:51 +0000

    Block and Door objects. Added some new features to Colors

--- Model/Colors.cs
+++ Model/Colors.cs
@@ -22,7 +22,14 @@ namespace Spectrum.Model
 
         public static Colors AllColors = new Colors(true, true, true, true, true, true);
         public static Colors NoColors = new Colors(false, false, false, false, false, false);
-
+		
+		public static Colors RedColor = new Colors(true, false, false, false, false, false);
+		public static Colors OrangeColor = new Colors(false, true, false, false, false, false);
+		public static Colors YellowColor = new Colors(false, false, true, false, false, false);
+		public static Colors GreenColor = new Colors(false, false, false, true, false, false);
+		public static Colors BlueColor = new Colors(false, false, false, false, true, false);
+		public static Colors PurpleColor = new Colors(false, false, false, false, false, true);
+		
         public Colors(bool r, bool o, bool y, bool g, bool b, bool p)
         {
             Bitstring = 0;
@@ -40,6 +47,10 @@ namespace Spectrum.Model
                 Bitstring |= (int)RawColors.Purple;
         }
 
+		public Colors(int bitstring) {
+			Bitstring = bitstring;
+		}
+
         // the player's viewing color is the receiver
         // the colors of the object to be seen is the parameter
         public bool contains(Colors c)
@@ -47,10 +58,17 @@ namespace Spectrum.Model
             return (Bitstring & c.Bitstring) == Bitstring;
         }
 
+		// Whether this represents just a singluar color like red, green, blue, etc
         public bool isSingularColor()
         {
-
+			return (Bitstring == Red || Bitstring == Orange || Bitstring == Yellow || Bitstring == Green || Bitstring == Blue || Bitstring == Purple);
         }
+
+		// Returns a new Colors object by combining components of this and 
+		// another Colors object
+		public bool combine(Colors c) {
+			return new Colors(Bitstring | c.Bitstring);
+		}
 
   
     }
