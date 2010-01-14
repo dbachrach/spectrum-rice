bad4cdf40ca9ab9fb567e4d4538d124283ad777d
nsb1@rice.edu <nsb1@rice.edu@f996e428-fef6-11de-89cc-8daf83ecd611>
2010-01-14 20:09:39 +0000

    Added Player and Polygon.cs

---
+++ Model/Player.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+
+namespace Spectrum.Model
+{
+    class Player : GameObject
+    {
+        public int TimesDied { get; set; }
+
+        public TimeSpan PlayTime { get; set; }
+
+        public GameObject Possession { get; set; }
+
+        public Player(int id, int timesDied, TimeSpan playTime, GameObject possession, Polygon polygon, Texture2D image)
+            : base(id, Colors.NoColors, polygon, image, new Vector2(0,0), true, new Vector2(0,0), null, null, false, false, null, null, true, null)
+        {
+            TimesDied = timesDied;
+            PlayTime = playTime;
+            Possession = possession;
+        }
+    }
+}
