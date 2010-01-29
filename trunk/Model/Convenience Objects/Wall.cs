fcd3f602a279dc77490c3697d54c43972ef0baa9
ahdustin <ahdustin@f996e428-fef6-11de-89cc-8daf83ecd611>
2010-01-29 01:11:11 +0000

    Added wall

---
+++ trunk/Model/Convenience Objects/Wall.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Spectrum.Model
+{
+    class Wall : SolidGround
+    {
+        public Wall() : base() {
+		}
+        public Wall(int w, int h)
+            : base(w,h)
+        {
+        }
+
+        protected override void DidLoadPhysicsBody()
+        {
+            geom.FrictionCoefficient = 0.0f; // TODO: Pick a good value for friction
+        }
+    }
+}
