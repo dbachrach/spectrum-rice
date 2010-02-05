5fe89b311ea35a867b2b8dc24646e3218218122d
ahdustin <ahdustin@f996e428-fef6-11de-89cc-8daf83ecd611>
2010-02-05 16:04:42 +0000

    Tortoise missed adding a couple files in view/.

---
+++ View/MenuItem.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+
+namespace Spectrum.View
+{
+    class MenuItem
+    {
+        private string _text = "";
+        private SpriteFont _font;
+        private Vector2 _position = Vector2.Zero;
+        private Color _baseColor;
+        private Color _selectedColor;
+
+        public bool Selected { get; set; }
+        public string Name { get; set; }
+
+        public MenuItem(string name, string text, SpriteFont font, Vector2 position, Color baseColor, Color selectedColor, bool selected)
+        {
+            Name = name;
+            _text = text;
+            _font = font;
+            _position = position;
+            _baseColor = baseColor;
+            _selectedColor = selectedColor;
+            Selected = selected;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            if (Selected)
+            {
+                spriteBatch.DrawString(_font, _text, _position, _selectedColor);
+            }
+            else
+            {
+                spriteBatch.DrawString(_font, _text, _position, _baseColor);
+            }
+        }
+    }
+}
