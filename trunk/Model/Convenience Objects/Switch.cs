7f0865076ac5cce9a569a88d5a50279dd64eb577
ahdustin <ahdustin@f996e428-fef6-11de-89cc-8daf83ecd611>
2010-02-15 20:06:41 +0000

    Added switch.

---
+++ trunk/Model/Convenience Objects/Switch.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Spectrum.Model.Convenience_Objects
+{
+    class Switch : GameObject
+    {
+        public Switch(Player p)
+            : base()
+        {
+            ImageName = "switchOff";
+            IsStatic = true;
+
+            Event e = new Event();
+            e.Type = EventType.Collision;
+            e.CollisionTarget = p;
+            e.Actions = new List<EventAction>();
+            EventAction act = new EventAction();
+            act.Receiver = this;
+            act.Property = Globals.ImageProperty;
+            act.Type = ActionType.Change;
+            act.Value = "switchOn_sm";
+
+            e.Actions.Add(act);
+
+            Events = new List<Event>();
+            Events.Add(e);
+        }
+    }
+}
