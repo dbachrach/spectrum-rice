a3b1e176c6c8bd9c267097c9b943bd69fedea3d9
ahdustin <ahdustin@f996e428-fef6-11de-89cc-8daf83ecd611>
2010-02-17 14:52:13 +0000

    Added the color bar. It scrolls okay. We need to make some adjustments to it, like making it indicate the right color, making the wedge be in the middle of a color rather than on the border of 2 colors. We need to clean up the names of the methods in the interface, and we need to make the image smaller. I made a IColorIndicator interface so we can easily switch from wheel to bar indicators. I also changed the default starting level to be demo.txt in Game1.cs at the top. For demo today to Warren, just remove "demo.txt" from the array "levels." I'll try to get most of these issues worked out today.

--- Game1.cs
+++ Game1.cs
@@ -34,7 +34,7 @@ namespace Spectrum
         public bool Paused { get; set; }
 
         private string levelPath = "Levels/";
-        private string[] levels = {"tutorial1.txt", "DrEvil.txt"};
+        private string[] levels = {"demo.txt", "tutorial1.txt", "DrEvil.txt"};
         private int levelIndex;
         private const int GameWidth = 1200;
         private const int GameHeight = 800;
---
+++ View/IColorIndicator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+using Spectrum.Model;
+
+namespace Spectrum.View
+{
+    interface IColorIndicator
+    {
+        void LoadContent(ContentManager manager, GraphicsDevice graphicsDevice);
+        void Update(GameTime gameTime);
+        void Draw(SpriteBatch spriteBatch);
+        void SetWheelColor(Colors colors);
+        void DidChangeColor(Colors colors, bool clockwise);
+
+        bool MoveBG { get; set; }
+    }
+}
