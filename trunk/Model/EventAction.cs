aaa3564bfc08e727712b08d319d61740d82f1740
nsb1@rice.edu <nsb1@rice.edu@f996e428-fef6-11de-89cc-8daf83ecd611>
2010-02-24 21:03:17 +0000

    Adding PathAnimate to the parser

--- trunk/Model/EventAction.cs
+++ trunk/Model/EventAction.cs
@@ -108,104 +108,109 @@ namespace Spectrum.Model
             }
         }
 
-        public virtual void Execute(List<EventAction> deferFuture, double curMs)
+        public void Execute(List<EventAction> deferFuture, double curMs)
         {
-            if(!HandleDelay(deferFuture, curMs))
+            if (!HandleDelay(deferFuture, curMs))
             {
-
-                /* TODO: All execution properties */
-                if (Special != null && !Special.Equals(""))
-                {
-                    if (Special.Equals(Globals.WinSpecial))
-                    {
-                        Console.WriteLine("WIN");
-                        Player p = (Player)this.Receiver;
-                        p.WinLevel();
-                    }
-                    else if (Special.Equals(Globals.LoseSpecial))
-                    {
-                        Console.WriteLine("LOSE");
-                        Player p = (Player)this.Receiver;
-                        p.LoseLevel();
-                    }
-                }
-                else if(Property.Equals(Globals.PositionProperty))
-                {
-                    switch (Type)
-                    {
-                        case ActionType.Change:
-                            ArrayList vals = (ArrayList)Value;
-                            Vector2 vec = new Vector2((float)((double)vals[0]), (float)((double)vals[1]));
-                            Receiver.body.Position += vec;
-                            break;
-                    }
-                }
-                else if (Property.Equals(Globals.ColorsProperty))
-                {
-                    switch (Type)
-                    {
-                        case ActionType.Change:
-                            Receiver.setVisibility(Colors.ColorsFromJsonArray((ArrayList)Value));
-                            break;
-                        case ActionType.AddColors:
-                            Receiver.setVisibility(Receiver.Visibility.Combine(Colors.ColorsFromJsonArray((ArrayList)Value)));
-                            break;
-                        case ActionType.RemoveColors:
-                            Receiver.setVisibility(Receiver.Visibility.Difference(Colors.ColorsFromJsonArray((ArrayList)Value)));
-                            break;
-                    }
-                }
-                else if (Property.Equals(Globals.PlayerTangibilityProperty))
-                {
-                    switch (Type)
-                    {
-                        case ActionType.Change:
-                            Receiver.PlayerTangibility = Colors.ColorsFromJsonArray((ArrayList)Value);
-                            break;
-                        case ActionType.AddColors:
-                            Receiver.PlayerTangibility = Receiver.Visibility.Combine(Colors.ColorsFromJsonArray((ArrayList)Value));
-                            break;
-                        case ActionType.RemoveColors:
-                            Receiver.PlayerTangibility = Receiver.Visibility.Difference(Colors.ColorsFromJsonArray((ArrayList)Value));
-                            break;
-                    }
-                }
-                else if (Property.Equals(Globals.ImageProperty))
-                {
-                    switch (Type)
-                    {
-                        case ActionType.Change:
-                            Receiver.ImageName = (string)Value;
-                            Receiver.LoadTexture();
-
-                            break;
-                    }
-                }
-                else if (Property.Equals("force"))
-                {
-                    switch (Type)
-                    {
-                        case ActionType.Change:
-                            ArrayList vals = (ArrayList)Value;
-                            Vector2 vec = new Vector2((float)((double)vals[0]), (float)((double)vals[1]));
-                            Console.WriteLine("Vector behav: {0}", vec);
-                            Receiver.body.ApplyForce(vec);
-
-                            break;
-                    }
-                }
-                else if (Property.Equals("whiteness"))
-                {
-                    switch (Type)
-                    {
-                        case ActionType.Change:
-                            Receiver.HasBecomeVisibleInAllColors = (bool)Value;
-                            Receiver.setVisibility(Colors.AllColors);
-                            break;
-                    }
-                }
+                // do this EventAction's specific action
+                InnerExecute(deferFuture, curMs);
 
                 HandleRepeats(deferFuture, curMs);
+            }
+        }
+
+        public virtual void InnerExecute(List<EventAction> deferFuture, double curMs)
+        {
+            /* TODO: All execution properties */
+            if (Special != null && !Special.Equals(""))
+            {
+                if (Special.Equals(Globals.WinSpecial))
+                {
+                    Console.WriteLine("WIN");
+                    Player p = (Player)this.Receiver;
+                    p.WinLevel();
+                }
+                else if (Special.Equals(Globals.LoseSpecial))
+                {
+                    Console.WriteLine("LOSE");
+                    Player p = (Player)this.Receiver;
+                    p.LoseLevel();
+                }
+            }
+            else if(Property.Equals(Globals.PositionProperty))
+            {
+                switch (Type)
+                {
+                    case ActionType.Change:
+                        ArrayList vals = (ArrayList)Value;
+                        Vector2 vec = new Vector2((float)((double)vals[0]), (float)((double)vals[1]));
+                        Receiver.body.Position += vec;
+                        break;
+                }
+            }
+            else if (Property.Equals(Globals.ColorsProperty))
+            {
+                switch (Type)
+                {
+                    case ActionType.Change:
+                        Receiver.setVisibility(Colors.ColorsFromJsonArray((ArrayList)Value));
+                        break;
+                    case ActionType.AddColors:
+                        Receiver.setVisibility(Receiver.Visibility.Combine(Colors.ColorsFromJsonArray((ArrayList)Value)));
+                        break;
+                    case ActionType.RemoveColors:
+                        Receiver.setVisibility(Receiver.Visibility.Difference(Colors.ColorsFromJsonArray((ArrayList)Value)));
+                        break;
+                }
+            }
+            else if (Property.Equals(Globals.PlayerTangibilityProperty))
+            {
+                switch (Type)
+                {
+                    case ActionType.Change:
+                        Receiver.PlayerTangibility = Colors.ColorsFromJsonArray((ArrayList)Value);
+                        break;
+                    case ActionType.AddColors:
+                        Receiver.PlayerTangibility = Receiver.Visibility.Combine(Colors.ColorsFromJsonArray((ArrayList)Value));
+                        break;
+                    case ActionType.RemoveColors:
+                        Receiver.PlayerTangibility = Receiver.Visibility.Difference(Colors.ColorsFromJsonArray((ArrayList)Value));
+                        break;
+                }
+            }
+            else if (Property.Equals(Globals.ImageProperty))
+            {
+                switch (Type)
+                {
+                    case ActionType.Change:
+                        Receiver.ImageName = (string)Value;
+                        Receiver.LoadTexture();
+
+                        break;
+                }
+            }
+            else if (Property.Equals("force"))
+            {
+                switch (Type)
+                {
+                    case ActionType.Change:
+                        ArrayList vals = (ArrayList)Value;
+                        Vector2 vec = new Vector2((float)((double)vals[0]), (float)((double)vals[1]));
+                        Console.WriteLine("Vector behav: {0}", vec);
+                        Receiver.body.ApplyForce(vec);
+
+                        break;
+                }
+            }
+            else if (Property.Equals("whiteness"))
+            {
+                switch (Type)
+                {
+                    case ActionType.Change:
+                        Receiver.HasBecomeVisibleInAllColors = (bool)Value;
+                        Receiver.setVisibility(Colors.AllColors);
+                        break;
+                }
             }
         }
     }
