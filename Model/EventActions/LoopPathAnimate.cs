75ddef981fbbf814a2806e5bcb0c90ebfc67f1fa
nsb1@rice.edu <nsb1@rice.edu@f996e428-fef6-11de-89cc-8daf83ecd611>
2010-03-09 19:14:16 +0000

    Adding animation files (oops)

---
+++ Model/EventActions/LoopPathAnimate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Spectrum.Model;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+
+
+namespace Spectrum.Model.EventActions
+{
+    class LoopPathAnimate : PathAnimate
+    {
+        public LoopPathAnimate() : base()
+        {
+        }
+
+        public override void SelectNextWaypoint()
+        {
+            Destination = Path[NextDestination];
+            NextDestination = (NextDestination + 1) % TotalSegments;
+        }
+    }
+}
