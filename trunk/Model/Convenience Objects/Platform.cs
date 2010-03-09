83ca69fa7e1a7473f0ddaa6388c0f5f029de3147
ahdustin <ahdustin@f996e428-fef6-11de-89cc-8daf83ecd611>
2010-03-09 20:27:12 +0000

    trying to generalize the use of numbers in platform code

--- trunk/Model/Convenience Objects/Platform.cs
+++ trunk/Model/Convenience Objects/Platform.cs
@@ -45,14 +45,20 @@ namespace Spectrum.Model
 
             GraphicsDevice g = Container.GameRef.GraphicsDevice;
 
-            int w = Math.Max((int) (_width * (1 / Scale)), 78*2);
-            int repeats = (w - 78*2) / 377;
-            if (w > 78*2)
+            const int leftWidth = 79;
+            const int rightWidth = 78;
+            const int minWidth = leftWidth + rightWidth;
+            const int centerWidth = 150;
+            const int allHeight = 1221;
+
+            int w = Math.Max((int) (_width * (1 / Scale)), minWidth);
+            int repeats = (w - minWidth) / centerWidth;
+            if (w > minWidth)
             {
-                w = 78*2 + repeats * 377;
+                w = minWidth + repeats * centerWidth;
             }
 
-            RenderTarget2D targ = new RenderTarget2D(g, w, 1221, 1, SurfaceFormat.Rgba1010102);
+            RenderTarget2D targ = new RenderTarget2D(g, w, allHeight, 1, SurfaceFormat.Rgba1010102);
             DepthStencilBuffer stenBuf = Globals.CreateDepthStencil(targ);
             // Cache the current depth buffer
             DepthStencilBuffer oldStenBuf = g.DepthStencilBuffer;
@@ -68,11 +74,11 @@ namespace Spectrum.Model
             Texture2D left = Container.GameRef.Content.Load<Texture2D>("platformEndLeft");
             b.Draw(left, new Vector2(0, 0), Color.White);
             Texture2D right = Container.GameRef.Content.Load<Texture2D>("platformEndRight");
-            b.Draw(right, new Vector2(w - 78, 0), Color.White);
+            b.Draw(right, new Vector2(w - rightWidth, 0), Color.White);
             Texture2D mid = Container.GameRef.Content.Load<Texture2D>("platformCenter");
             for (int cnt = 0; cnt < repeats; cnt++)
             {
-                b.Draw(mid, new Vector2(78 + (377 * cnt), 0), Color.White);
+                b.Draw(mid, new Vector2(leftWidth + (centerWidth * cnt), 0), Color.White);
             }
             b.End();
 
