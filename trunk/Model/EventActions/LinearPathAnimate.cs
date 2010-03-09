c6ebb5acbb3f34439c3bf37bfb59a43c5edaa6cf
nsb1@rice.edu <nsb1@rice.edu@f996e428-fef6-11de-89cc-8daf83ecd611>
2010-03-09 19:14:16 +0000

    Adding animation files (oops)

---
+++ trunk/Model/EventActions/LinearPathAnimate.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Spectrum.Model;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+
+
+namespace Spectrum.Model.EventActions
+{
+    class LinearPathAnimate : PathAnimate
+    {
+        public LinearPathAnimate()
+            : base()
+        {
+        }
+
+        public override void SelectNextWaypoint()
+        {
+            if (NextDestination >= TotalSegments - 1)
+            {
+                Receiver.body.Position = Path[0];
+                Destination = Path[1];
+                NextDestination = 2;
+            }
+            else
+            {
+                Destination = Path[NextDestination];
+                NextDestination = (NextDestination + 1) % TotalSegments;
+            }
+
+        }
+    }
+}
