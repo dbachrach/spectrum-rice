6164180ff50fe914031ff8956e702a06226de92b
ahdustin <ahdustin@f996e428-fef6-11de-89cc-8daf83ecd611>
2010-03-22 19:46:00 +0000

    Switcharoo

--- SpectrumGame.cs
+++ SpectrumGame.cs
@@ -35,9 +35,9 @@ namespace Spectrum
 
         private string levelPath = "Levels/";
         private string levelExtension = ".txt";
-        private string[] levels = {"TrainingDay", "DrEvil","CombinationPizzaHut", "cloudy", "DodgeDuckDip", "CloudKnives"};
+        private string[] levels = {"TrainingDay", "DrEvil","CombinationPizzaHut", "cloudy", "DodgeDuckDip", "CloudKnives", "Switcharoo"};
         // TODO: Remove this later since its in the level file itself
-        public string[] levelsPresentable = { "Training Day", "Dr. Evil's Quarters", "Combination Pizza Hut", "Cloudy with a chance of death", "Dodge, Duck, Dip, Dive and Dodge", "Clouds Run Faster With Knives" };
+        public string[] levelsPresentable = { "Training Day", "Dr. Evil's Quarters", "Combination Pizza Hut", "Cloudy with a chance of death", "Dodge, Duck, Dip, Dive and Dodge", "Clouds Run Faster With Knives", "Switcharoo"};
         private int levelIndex;
 
         private SplashScreen splash;
