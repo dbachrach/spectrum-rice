897e2de789eb15aafbe9c56a5347ecfc8f6e9e3e
nsb1@rice.edu <nsb1@rice.edu@f996e428-fef6-11de-89cc-8daf83ecd611>
2010-04-05 21:04:52 +0000

    push buttons and music.

--- Model/Convenience Objects/PushButton.cs
+++ Model/Convenience Objects/PushButton.cs
@@ -1,33 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
 
 namespace Spectrum.Model.Convenience_Objects
 {
-    class PushButton : GameObject
+    class PushButtonBox : GameObject
     {
-        public PushButton(Player p)
+        public PushButtonBox(Player p)
             : base()
         {
-            ImageNames = new List<string>() {"switchBox" };
+            ImageNames = new List<string>() {"switchBox"};
+            Scale = 0.15f;
             IsStatic = true;
-            Scale = 0.1f;
+        }
+    }
 
+    class PushButtonPusher : GameObject
+    {
+        public PushButtonPusher(Player p)
+            : base()
+        {
+            ImageNames = new List<string>() { "switchBoxButton" };
+            Scale = 0.15f;
+            IsStatic = true;
+            
             Event e = new Event();
             e.Type = EventType.Collision;
             e.CollisionTarget = p;
             e.Actions = new List<EventAction>();
             EventAction act = new EventAction();
             act.Receiver = this;
-            act.Property = Globals.ImageProperty;
-            act.Type = ActionType.Change;
-            act.Value = "switchBoxButton"; // TODO: Make this a corrrect event action
+            act.Property = Globals.PositionProperty;
+            act.Type = ActionType.Increment;
+            act.Value = new ArrayList() { 0.0, 5.0 };
 
             e.Actions.Add(act);
 
             Events = new List<Event>();
             Events.Add(e);
+            
         }
     }
 }
