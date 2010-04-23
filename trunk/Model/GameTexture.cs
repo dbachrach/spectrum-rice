56651ad8ce297c80d851663898f573ff5ab7636f
nsb1@rice.edu <nsb1@rice.edu@f996e428-fef6-11de-89cc-8daf83ecd611>
2010-04-23 19:50:50 +0000

    added hint dialog message

--- trunk/Model/GameTexture.cs
+++ trunk/Model/GameTexture.cs
@@ -86,7 +86,7 @@ namespace Spectrum.Model
                 {  
                     AssetCount = 6;
                 }
-                else if (asset.Equals("hint") || asset.Equals("textBoxMix"))
+                else if (asset.Equals("hint") || asset.Equals("textBoxMix") || asset.Equals("textBoxBox"))
                 {
                     AssetCount = 1;
                 }
